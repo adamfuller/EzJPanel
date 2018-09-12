fb22e809b19d86a957c465557b642216eb5c9013
alfuller16 <alfuller16@gmail.com>
2018-09-12 14:06:56 -0500

    initial commit

---
+++ Test.java
@@ -0,0 +1,31 @@
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Test extends EzJPanel{
+    private Color color = new Color(30, 130, 20);
+
+    public static void main(String ags[]){
+        Test t = new Test();
+    }
+
+    @Override
+    protected void setup(){
+        stopLooping();
+        rename("HHHHHHH");
+    }
+
+    public Test(int width, int height, String title){
+        super(width, height, title);
+    }
+    public Test(){
+        super();
+    }
+
+    @Override
+    protected void draw(Graphics g){
+
+        g.setColor(color);
+        g.fillRect(50, 50, 100, 100);
+    }
+
+}
